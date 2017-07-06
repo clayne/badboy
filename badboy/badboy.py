70f749a0b61c9cffc828c2532063eeea86925fb1
adam nunez <adam.a.nunez@gmail.com>
2017-07-06 00:09:42 -0500

    moving data to namedtuple

--- badboy/badboy.py
+++ badboy/badboy.py
@@ -2,9 +2,9 @@
 
 from praw import Reddit
 from textwrap import wrap
-from collections import deque
 from .badlist import THE_LIST
 from operator import itemgetter
+from collections import namedtuple
 from .credentials import setup_credentails, load_credentials
 
 # TODO Real results, not just text (pie chart)
@@ -15,15 +15,16 @@ MAX_FAVS = 5
 LEFT_DISPLACE = 15
 HEADER_LENGTH = 70
 
+RedditUserHistory = namedtuple('RedditUserHistory', 'total bad general best')
+
 def review( reddit_iter, depth, numb_top_entries=0 ):
 
-    badRecord, generalRecord, best = {}, {}, deque(maxlen=numb_top_entries)
-
+    badRecord, generalRecord, best = {}, {}, []
     for entry in reddit_iter.new(limit=depth):
 
         if numb_top_entries != 0 and (not best or entry.ups > best[0][0]):
-            best.append((entry.ups, entry))
-            best = deque( sorted(best, key=lambda x: x[0]), maxlen=numb_top_entries )
+            best.append( (entry.ups, entry) )
+            best = sorted(best, key=lambda x: x[0], reverse=True)[:numb_top_entries]
 
         sub_name = entry.subreddit.display_name
         generalRecord[sub_name] = generalRecord.get(sub_name, 0) + 1
@@ -31,9 +32,8 @@ def review( reddit_iter, depth, numb_top_entries=0 ):
         if sub_name.lower() in THE_LIST:
             badRecord[sub_name] = badRecord.get(sub_name, 0) + 1
 
-    checked = sum([v for v in generalRecord.values()])
-
-    return checked, badRecord, generalRecord, [pair[1] for pair in best]
+    return RedditUserHistory( sum(generalRecord.values()), badRecord,
+        generalRecord, [pair[1] for pair in best] )
 
 def connect():
     creds = load_credentials()
@@ -46,10 +46,10 @@ def connect():
     return reddit
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-# Terminal
+# Terminal Display
 
 def print_best_comments( best ):
-    for comment in reversed(best):
+    for comment in best:
         print( "Top Comment in " + comment.subreddit.display_name + \
             "(" + str(comment.ups) + "):\n" + '\n'.join(wrap(comment.body,HEADER_LENGTH)), end='\n\n')
 
@@ -70,15 +70,15 @@ def print_fav_subject( checked, record, title ):
 
 def badboy_terminal(reddit, user, depth, top):
 
-    checked, badRecord, generalRecord, best = review( reddit.redditor(user).comments, depth, top )
+    comment_history = review( reddit.redditor(user).comments, depth, top )
 
-    print_bad_subject( checked, badRecord, "Bad Comments" )
-    print_fav_subject( checked, generalRecord, "Favorite Places to Comment" )
-    print_best_comments( best )
+    print_bad_subject( comment_history.total, comment_history.bad, "Bad Comments" )
+    print_fav_subject( comment_history.total, comment_history.general, "Favorite Places to Comment" )
+    print_best_comments( comment_history.best )
 
-    checked, badRecord, generalRecord, best = review( reddit.redditor(user).submissions, depth )
+    post_history = review( reddit.redditor(user).submissions, depth )
 
-    print_bad_subject( checked, badRecord, "Bad Submissions" )
-    print_fav_subject( checked, generalRecord, "Favorite Subs to Submit to" )
+    print_bad_subject( post_history.total, post_history.bad, "Bad Submissions" )
+    print_fav_subject( post_history.total, post_history.general, "Favorite Subs to Submit to" )
 
 
